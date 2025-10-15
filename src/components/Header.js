d040ee04c18183df1e03617dcd2c7a8e7453e55a
Ndet0 <festusndet00@gmail.com>
2025-10-15 15:22:48 +0300

    Adds App.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header({ onDarkModeClick, isDarkMode = false }) {
+  return (
+    <header>
+      <h2>Shopster</h2>
+      <button onClick={onDarkModeClick}>
+        {isDarkMode ? "Dark" : "Light"} Mode
+      </button>
+    </header>
+  );
+}
+
+export default Header;
